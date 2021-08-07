cf2b55d53dcc745ba7b0c8b9b657603591212806
Uniday Studio <guilhermegt13@gmail.com>
2021-08-06 21:16:40 -0300

    Persistindo os Dados

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,27 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(second_saved_item.text, 'O segundo item')
+        
+
+
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
