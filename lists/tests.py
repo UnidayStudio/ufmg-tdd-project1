42a1afe531370c54e8f2cbfabd34276f6f62290f
Uniday Studio <guilhermegt13@gmail.com>
2021-08-06 21:12:23 -0300

    Processando Requisição POST

--- lists/tests.py
+++ lists/tests.py
@@ -14,6 +14,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
 
