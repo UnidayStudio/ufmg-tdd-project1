b57a0a2a25df24837e7126af9563e68a616ebd78
Uniday Studio <guilhermegt13@gmail.com>
2021-08-09 23:31:14 -0300

    Melhoria 2: Removendo Sleeps para Evitar Não Determinismo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 5
+
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -10,6 +14,20 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element_by_id('id_list_table')  
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException) as e:  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise e  
+                time.sleep(0.5)
+
 
     def test_can_start_a_list_and_retrieve_it_later(self): 
     
