a0b6d220e6b1a353b8a5a33fd29b7731dad4a165
Uniday Studio <guilhermegt13@gmail.com>
2021-08-09 23:44:32 -0300

    Novo Projeto: Atualizando o Template

--- lists/tests.py
+++ lists/tests.py
@@ -36,7 +36,7 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
-        
+
 
 class ListAndItemModelsTest(TestCase):
 
@@ -82,6 +82,12 @@ class NewListTest(TestCase):
 
 
 class ListViewTest(TestCase):
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
 
     def test_uses_list_template(self):
         my_list = List.objects.create()
